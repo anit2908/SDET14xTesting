481cc810334bc1327da6e0a1a0a99f48d36d6df8
anit2908 <maharanaanit@gmail.com>
2025-09-16 14:40:36 +0530

    Arithmetic and Assignment

---
+++ src/ex_02_Operators/Arithmetic.java
@@ -0,0 +1,20 @@
+package ex_02_Operators;
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 30;
+        float c = 20.0f;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(b/a);
+        System.out.println(a%b);
+    }
+
+
+
+}
