0d6908aa3a9d70facebcd372e87de565af7dc9d5
anit2908 <maharanaanit@gmail.com>
2025-09-16 15:02:48 +0530

    Logical operator added

---
+++ src/ex_02_Operators/Logical.java
@@ -0,0 +1,17 @@
+package ex_02_Operators;
+
+public class Logical {
+
+    public static void main(String[] args) {
+
+        boolean a = true;
+        boolean b = false;
+        boolean c = a && b;
+        boolean d = a || b;
+        boolean e = !a;
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
