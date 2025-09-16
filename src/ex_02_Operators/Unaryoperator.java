8ba954da6eac11c5d8b3aa0f51909043493cb132
anit2908 <maharanaanit@gmail.com>
2025-09-16 14:57:18 +0530

    Arithmetic and Assignment

---
+++ src/ex_02_Operators/Unaryoperator.java
@@ -0,0 +1,16 @@
+package ex_02_Operators;
+
+public class Unaryoperator {
+
+    public static void main(String[] args) {
+
+        int a =100;
+
+        System.out.println( a+10);
+
+        int b = 100;
+        int c = b+10;
+        System.out.println(c);
+        System.out.println(b);
+    }
+}
